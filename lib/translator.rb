096d44ac7191fc18d1a6bc4757583535fbab6516
saidmysr20@gmail.com <saidmysr20@gmail.com>
2018-08-29 21:55:05 +0000

    automatically backed up by learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -9,7 +9,7 @@ def load_library(file_path)
   emoticons.each do |emoticon, meaning|
     meaning.each do |english_emo|
       english_emo.each do |x|
-    binding.pry
+        binding.pry
     
     end
       
