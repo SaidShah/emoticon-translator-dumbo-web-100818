9e9edab09593b78cff6ff15c88fb732fb08481b7
saidmysr20@gmail.com <saidmysr20@gmail.com>
2018-08-29 21:39:07 +0000

    automatically backed up by learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,7 +1,8 @@
 # require modules here
 
 def load_library(file_path)
-   new_hash = "./emoticon-translator-dumbo-web-100818/lib/emoticons.yml"
+ #  new_hash = "./emoticon-translator-dumbo-web-100818/lib/emoticons.yml"
+ emoticons = YAML.load_file(file_path)
 end
 
 def get_japanese_emoticon
