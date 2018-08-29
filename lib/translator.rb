79620256035d5b709a75991588a64a659c892d24
saidmysr20@gmail.com <saidmysr20@gmail.com>
2018-08-29 22:05:16 +0000

    automatically backed up by learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -7,9 +7,9 @@ def load_library(file_path)
  new_hash = {}
  emoticons = YAML.load_file(file_path)
       new_hash["get_meaning"]={}
-      new_hash[":get_emoticon"]={}
+      new_hash["get_emoticon"]={}
   emoticons.each do |emoticon, meaning|
-    new_hash[:get_meaning][meaning[1]]=emoticon
+    new_hash[":get_meaning"][meaning[1]]=emoticon
     new_hash[:get_emoticon][meaning[0]]=meaning[1]
   end
   new_hash
