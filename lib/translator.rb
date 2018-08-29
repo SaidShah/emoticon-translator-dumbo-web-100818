99388a318f43f16660a8c0f86fa2634bc81232a3
saidmysr20@gmail.com <saidmysr20@gmail.com>
2018-08-29 22:00:12 +0000

    automatically backed up by learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -6,11 +6,11 @@ require "pry"
 def load_library(file_path)
  new_hash = {}
  emoticons = YAML.load_file(file_path)
- 
-  emoticons.each do |emoticon, meaning|
-    
       new_hash[:get_meaning]={}
       new_hash[:get_emoticon]={}
+  emoticons.each do |emoticon, meaning|
+    
+     
   end
   new_hash
   binding.pry
